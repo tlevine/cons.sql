dfc1c363cf886fc6e3436d84bc8d38336dc9c337
Thomas Levine <occurrence@thomaslevine.com>
2013-09-25 10:06:06 -0700

    function runs

--- cons.sql
+++ cons.sql
@@ -1,9 +1,13 @@
-CREATE FUNCTION cons(value, key) RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION cons(TEXT, INTEGER)
+RETURNS INTEGER AS $$
+DECLARE
+  key ALIAS FOR $2;
+  value ALIAS FOR $1;
 BEGIN
   CREATE TABLE IF NOT EXISTS "__cons" (
     "key" INTEGER PRIMARY KEY NOT NULL,
     "value" TEXT NOT NULL,
-    "nextKey" INTEGER REFERENCES "__cons" ("key"),
+    "nextKey" INTEGER REFERENCES "__cons" ("key")
   );
 
   INSERT INTO "__cons" ("value", "nextKey")
