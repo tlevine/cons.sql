11852c8c3f2d91eb6e6e792a05d5421cb01ad77f
Thomas Levine <occurrence@thomaslevine.com>
2013-09-25 10:34:57 -0700

    cons function

--- cons.sql
+++ cons.sql
@@ -1,17 +1,23 @@
+DROP TABLE IF EXISTS "__cons";
+CREATE TABLE "__cons" (
+  "thisKey" SERIAL PRIMARY KEY NOT NULL,
+  "value" TEXT NOT NULL,
+  "nextKey" INTEGER -- REFERENCES "__cons" ("thisKey")
+);
+
 CREATE OR REPLACE FUNCTION cons(TEXT, INTEGER)
 RETURNS INTEGER AS $$
 DECLARE
   nextKey ALIAS FOR $2;
   value ALIAS FOR $1;
 BEGIN
-  CREATE TABLE IF NOT EXISTS "__cons" (
-    "thisKey" INTEGER PRIMARY KEY NOT NULL,
-    "value" TEXT NOT NULL,
-    "nextKey" INTEGER REFERENCES "__cons" ("thisKey")
-  );
-
   INSERT INTO "__cons" ("value", "nextKey")
-  VALUES (value, nextKkey)
-  RETURNING "thisKey";
+  VALUES (value, nextKey);
+
+  -- http://stackoverflow.com/questions/2944297/postgresql-function-for-last-inserted-id
+  RETURN LASTVAL();
 END;
 $$ LANGUAGE plpgsql;
+
+select cons('abc', cons('ggg', NULL));
+select * from "__cons";
