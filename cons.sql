52dfb51df12f0b6d5e41271ea11aaa30dc8b2f4e
Thomas Levine <occurrence@thomaslevine.com>
2013-09-25 10:08:08 -0700

    change names

--- cons.sql
+++ cons.sql
@@ -1,17 +1,17 @@
 CREATE OR REPLACE FUNCTION cons(TEXT, INTEGER)
 RETURNS INTEGER AS $$
 DECLARE
-  key ALIAS FOR $2;
+  nextKey ALIAS FOR $2;
   value ALIAS FOR $1;
 BEGIN
   CREATE TABLE IF NOT EXISTS "__cons" (
-    "key" INTEGER PRIMARY KEY NOT NULL,
+    "thisKey" INTEGER PRIMARY KEY NOT NULL,
     "value" TEXT NOT NULL,
-    "nextKey" INTEGER REFERENCES "__cons" ("key")
+    "nextKey" INTEGER REFERENCES "__cons" ("thisKey")
   );
 
   INSERT INTO "__cons" ("value", "nextKey")
-  VALUES (value,key)
-  RETURNING "key";
+  VALUES (value, nextKkey)
+  RETURNING "thisKey";
 END;
 $$ LANGUAGE plpgsql;
