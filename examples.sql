da31dffc63081d6d1508c5648034fc7fcd54c944
Thomas Levine <occurrence@thomaslevine.com>
2013-10-13 15:48:45 -0700

    separate lib and examples

---
+++ examples.sql
@@ -0,0 +1,15 @@
+SELECT cons('a',cons('b',cons('c',cons('d',cons('e', NULL)))));
+SELECT cons('foo',cons('bar',cons('baz', NULL)));
+SELECT cons('i',cons('am',cons('a',cons('banana',NULL))));
+SELECT head(4);
+SELECT tail(5);
+SELECT drop(5, 2);
+SELECT init(4);
+SELECT init(4);
+SELECT last(4);
+SELECT copy(4);
+SELECT copy(4);
+SELECT cat(8,4);
+SELECT * FROM toColumn(5);
+
+
